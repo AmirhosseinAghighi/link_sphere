36a8671bd319316227aa504cfbc58f9ffae6cf10
amirhosseinaghighi <amirhossein.aghighi2@gmail.com>
2024-06-02 21:00:16 +0330

    gson and DAO injector methods

--- core/startupHandlers/Injector.java
+++ core/startupHandlers/Injector.java
@@ -1,5 +1,7 @@
 package org.linkSphere.core.startupHandlers;
 
+import org.linkSphere.core.Sphere;
+import org.linkSphere.database.DAO;
 import org.linkSphere.util.Logger;
 
 import java.lang.reflect.Field;
@@ -12,6 +14,32 @@ public class Injector {
             loggerField = clazz.getDeclaredField("logger");
             loggerField.setAccessible(true);
             loggerField.set(clazz, Logger.getLogger());
+        } catch (NoSuchFieldException e) {
+            throw e;
+        } catch (IllegalAccessException e) {
+            throw e;
+        }
+    }
+
+    public static void injectGson(Class clazz) throws NoSuchFieldException, IllegalAccessException {
+        Field gsonField = null;
+        try {
+            gsonField = clazz.getDeclaredField("gson");
+            gsonField.setAccessible(true);
+            gsonField.set(clazz, Sphere.getGson());
+        } catch (NoSuchFieldException e) {
+            throw e;
+        } catch (IllegalAccessException e) {
+            throw e;
+        }
+    }
+
+    public static void injectDAO(Class clazz) throws NoSuchFieldException, IllegalAccessException {
+        Field daoField = null;
+        try {
+            daoField = clazz.getDeclaredField("dao");
+            daoField.setAccessible(true);
+            daoField.set(clazz, DAO.getInstance());
         } catch (NoSuchFieldException e) {
             throw e;
         } catch (IllegalAccessException e) {
