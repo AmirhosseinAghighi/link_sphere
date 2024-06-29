4346ec6dbbe07a7012e2ec5b05081995a23bd6e3
amirhosseinaghighi <amirhossein.aghighi2@gmail.com>
2024-06-30 03:04:33 +0330

    document lastest added endpoints

--- src/main/java/app/main.java
+++ src/main/java/app/main.java
@@ -90,6 +90,54 @@ class hi {
             - Response: json responses;
                 | 403: {"code: 403", "Not authorized"} => if access token is invalid
                 | 400: {"code: 403", "Bad Request"} => if uploaded file is not png or jpg
-                | 403: {"code: 500", "Internal Server Error"} => if there is problem in saving file
+                | 500: {"code: 500", "Internal Server Error"} => if there is problem in saving file
 
+        - GET `/profile/{userID}/photo`: Get user profile picture
+            |
+            - Request Body Schema: None
+            |
+            - Response: (200) Binary file ( profile picture in jpg or png format )
+
+        - POST `/profile/banner`: updating user banner picture
+            |
+            - Request Body Schema: binary file ( "image" as key and image file as value )
+            |
+            - Response: json responses;
+                | 403: {"code: 403", "Not authorized"} => if access token is invalid
+                | 400: {"code: 403", "Bad Request"} => if uploaded file is not png or jpg
+                | 500: {"code: 500", "Internal Server Error"} => if there is problem in saving file
+
+        - GET `/profile/{userID}/banner`: Get user banner picture
+            |
+            - Request Body Schema: None
+            |
+            - Response: (200) Binary file ( banner picture in jpg or png format )
+
+        - POST `/profile/update`: updating profile information such as firstname, lastname, nickname, country and bio
+            |
+            - Request Body Schema: {"firstName": "test", "lastName": "test", "nickname": "test", countryCode: 364, "bio": "test"}
+                | firstName, lastName, nickname, countryCode are required for first time ( it will create the profile row in db )
+            |
+            - Response: json responses;
+                | 400: {"code": 403, message: "Bad Request"} => if required field are null or empty, or they're exceeding maximum limit.
+                | 404: {"code": 404, message: "User not found."} => if user not found ( why it should happen really ?!
+
+        - POST `/profile/jobs`: create job on profile
+            |
+            - Request Body Schema:
+                | if we want to create new one that we are currently working: {"title": "Front-end Developer", "company": 1, "startDate": 1593439663, "description": "test"}
+                | if we want to create new one that we aren't currently working: {"title": "Front-end Developer", "company": 1, "startDate": 1593439663, "endDate": 589043279852, "description": "test"}
+            |
+            - Response: json responses:
+                | 400: {"code": 400, message: Bad Request"}
+                | 404: {"code": 404, message: "%s not found"} -> %s can be User (profile) or Company or job ( if we are editing )
+
+        - PUT `/profiles/jobs/{jobID}`: update existing job by id
+            |
+            - Request Body Schema:
+                | if we want to edit existing one ( only edited fields: {"title": "Back-end Developer", "description": "test", id = 321}
+            |
+            - Response: json responses:
+                | 400: {"code": 400, message: Bad Request"}
+                | 404: {"code": 404, message: "%s not found"} -> %s can be User (profile) or Company or job ( if we are editing )
  */
