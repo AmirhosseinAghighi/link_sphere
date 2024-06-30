1672137c9f375948899c624de8ed8bc4aba88d3c
amirhosseinaghighi <amirhossein.aghighi2@gmail.com>
2024-06-30 22:23:14 +0330

    refactoring skills system

--- src/main/java/app/views/profile/Skills.java
+++ src/main/java/app/views/profile/Skills.java
@@ -1,7 +1,6 @@
 package app.views.profile;
 
 import app.database.schema.Skill;
-import app.database.schema.User;
 import app.services.AuthService;
 import app.services.UserService;
 import com.google.gson.Gson;
@@ -9,12 +8,15 @@ import io.jsonwebtoken.Claims;
 import org.hibernate.exception.ConstraintViolationException;
 import org.linkSphere.annotations.UseLogger;
 import org.linkSphere.annotations.http.Endpoint;
+import org.linkSphere.annotations.http.Get;
 import org.linkSphere.annotations.http.Post;
 import org.linkSphere.annotations.useGson;
 import org.linkSphere.http.dto.Req;
 import org.linkSphere.http.dto.Res;
 import org.linkSphere.security.JWT;
 import org.linkSphere.util.Logger;
+
+import java.util.List;
 
 @Endpoint("/profile")
 @useGson
@@ -41,6 +43,19 @@ public class Skills {
         } catch (ConstraintViolationException | IllegalArgumentException e) {
             res.sendError(400, "Bad Request");
         }
+    }
+
+    @Get("/{userID}/skills")
+    public void getSkills(Req req, Res res) {
+        long userID = Long.parseLong(req.getDynamicParameters().get("userID"));
+        if (!UserService.doesUserExist(userID)) {
+            res.sendError(404, "User not found.");
+            return;
+        }
+
+        List<Skill> skills = UserService.getUserSkillsById(userID);
+        res.send(200, "{\"code\": 200, \"skills\": " + skills.toString() + "}");
+        // TODO: Find out why gson.toJson(jobs) will throw null exception ( an exception with null message )
     }
 
     @Post("/skills/{skillID}/remove")
